5aed35cf871e5f1880bc24ca30382c2c9b822f5f
shruti881994 <57830840+shruti881994@users.noreply.github.com>
2024-01-31 15:18:25 +0530

    Add files via upload

---
+++ assets/css/script.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){
+$("header").append('<nav class="navbar navbar-expand-lg navbar-light fixed-top navbar-shrink" id="nav-bar" data-spy="affix"data-offset-top="50"><div class="container"><a class="navbar-brand" href="index.html"><img src="assets/images/logo.png" width="120px"></a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse" id="navbarSupportedContent"><ul class="navbar-nav ml-auto"><li class="nav-item"><a class="nav-link link-hvr" href="index.html">Home</a></li><li class="nav-item dropdown "><a class="nav-link  link-hvr " data-target="aboutus.html" href="aboutus.html" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">About Us</a><ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"><li class="dropdown-submenu "><a class="dropdown-item" href="terms-and-conditions.html">Terms & Conditions</a></li></ul></li><li class="nav-item dropdown "><a class="nav-link  link-hvr " data-target="destination.html" href="destination.html" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">Destination</a><ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"><li class="dropdown-submenu "><a class="dropdown-item dropdown-toggle" href="">Karnataka</a><ul class="dropdown-menu" id="prgrm"><li class="dropdown-submenu"><a class="dropdown-item " href="kumara-parvatha.html"> Kumara Parvatha Trek</a></li><li class="dropdown-submenu"> <a class="dropdown-item " href="kodachadri-trek.html">Kodachadri Trek </a></li><li class="dropdown-submenu"><a class="dropdown-item " href="kudaremukh-trek.html">Kudremukh Trek</a></li><li class="dropdown-submenu"> <a class="dropdown-item " href="chikmangalur-camping.html">Chikmangalur Camping</a></li><li class="dropdown-submenu"> <a class="dropdown-item " href="gokarna-trek.html">Gokarna Trek</a></li><li class="dropdown-submenu"> <a class="dropdown-item " href="tadiandamol-trek.html">Tadiandamol Trek</a></li><li class="dropdown-submenu"> <a class="dropdown-item " href="gandikota-camping-and-adventure-activities-blr-to-blr.html">Gandikota Adventure(Bangalore – Banagalore)</a></li><li class="dropdown-submenu"> <a class="dropdown-item " href="antargange-trek.html">Anthargange Trek</a></li><li class="dropdown-submenu"> <a class="dropdown-item " href="makalidurga-trek.html">Makalidurga Trek</a></li><li class="dropdown-submenu"> <a class="dropdown-item " href="kabbaladurga-trek.html">Kabbaladurga Trek</a></li><li class="dropdown-submenu"> <a class="dropdown-item " href="savandurga-trek.html">Savandurga Trek</a></li><li class="dropdown-submenu"> <a class="dropdown-item " href="skandagiri-sunrise-trek.html">Skandagiri Sunrise Trek</a></li><li class="dropdown-submenu"> <a class="dropdown-item " href="uttari-betta-trek.html">Uttari Betta Trek</a></li></ul></li><li class="dropdown-submenu "><a class="dropdown-item dropdown-toggle" href="">Andra Pradesh</a><ul class="dropdown-menu" id="prgrm"><li class="dropdown-submenu"><a class="dropdown-item " href="gandikota-adventure.html"> Gandikota Adventure</a></li></ul></li><li class="dropdown-submenu "><a class="dropdown-item dropdown-toggle" href="">Goa</a><ul class="dropdown-menu" id="prgrm"><li class="dropdown-submenu"><a class="dropdown-item " href="dudhsagar-trek.html">Dudhsagar Trek</a></li></ul></li><li class="dropdown-submenu "><a class="dropdown-item dropdown-toggle" href="">Maharastra</a><ul class="dropdown-menu" id="prgrm"><li class="dropdown-submenu"><a class="dropdown-item " href="kalsubai-monsoon-trek.html">Kalsubai Trek</a></li><li class="dropdown-submenu"><a class="dropdown-item " href="rajmachi-trek.html">Rajmachi Trek</a></li></ul></li><li class="dropdown-submenu "><a class="dropdown-item dropdown-toggle" href="">Jammu & Kashmir</a><ul class="dropdown-menu" id="prgrm"><li class="dropdown-submenu"><a class="dropdown-item " href="great-lakes-of-kashmir.html">Great Lakes of Kashmir</a></li><li class="dropdown-submenu"><a class="dropdown-item " href="tarsar-marsar-trek.html">Tarmar Marsar Trek</a></li><li class="dropdown-submenu"><a class="dropdown-item " href="markha-valley-trek.html">Markha Valley Trek</a></li><li class="dropdown-submenu"><a class="dropdown-item " href="kang-yatze-ii.html">Kang Yatze II</a></li></ul></li><li class="dropdown-submenu "><a class="dropdown-item dropdown-toggle" href="">Utharakhand</a><ul class="dropdown-menu" id="prgrm"><li class="dropdown-submenu"><a class="dropdown-item" href="pangarchulla-peak-trek.html">Pangarchulla Peak Trekking</a></li></ul></li><li class="dropdown-submenu "><a class="dropdown-item dropdown-toggle" href="">Himachal Pradesh</a><ul class="dropdown-menu" id="prgrm"><li class="dropdown-submenu"><a class="dropdown-item " href="beas-kund-trek.html">Beas Kund</a></li><li class="dropdown-submenu"><a class="dropdown-item " href="bhrigu-lake-trek.html">Brighu Lake Trek</a></li><li class="dropdown-submenu"><a class="dropdown-item " href="hampta-pass-trek.html">Hampta Pass Trek</a></li></ul></li><li class="dropdown-submenu "><a class="dropdown-item" href="nepal-trek.html">Nepal</a></li><li class="dropdown-submenu "><a class="dropdown-item" href="sikkim-trek.html">Sikkim</a></li></ul></li><li class="nav-item"><a class="nav-link link-hvr" href="gallery.html">Gallery</a></li><li class="nav-item"><a class="nav-link link-hvr" href="contact.html">Contact us</a></li></ul> <a href="" class="btn btn-success my-2 my-sm-0 ml-5">Join Us</a></div></div></nav>');
+$("footer").append('<div class="container"><img src="assets/images/logo.png" alt="log" class="m-auto d-block bg-white"><div class="row"><div class="col-lg-12 col-md-12 col-12"><ul class="footer_links"><li><a href="index.html">Home</a></li><li><a href="aboutus.html">About us</a></li><li><a href="destination.html">Destination</a></li><li><a href="gallery.html">Gallery</a></li><li><a href="contact.html">Contact us</a></li></ul><div class="social"><a href=""><i class="fa fa-phone phone" aria-hidden="true"></i></a><a href=""><i class="fa fa-whatsapp whatsapp" aria-hidden="true"></i></a><a href=""><i class="fa fa-facebook facebook" aria-hidden="true"></i></a><a href=""><i class="fa fa-instagram insta" aria-hidden="true"></i></a></div></div></div></div>');
+ $('.date').datepicker({
+   multidate: true,
+  minDate: 0,
+  dateFormat: "dd/mm/yy",
+  todayHighlight: true,
+  autoclose: true,
+  startDate: '-2d',
+
+});
+});
+$(document).ready(function(){
+    jQuery(window).scroll(startCounter);
+function startCounter() {
+    var hT = jQuery('.counter-1').offset().top,
+        hH = jQuery('.counter-1').outerHeight(),
+        wH = jQuery(window).height();
+    if (jQuery(window).scrollTop() > hT+hH-wH) {
+        jQuery(window).off("scroll", startCounter);
+        jQuery('.counts').each(function () {
+            var $this = jQuery(this);
+            jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                duration: 2000,
+                easing: 'swing',
+                step: function () {
+                    $this.text(Math.ceil(this.Counter) + '+');
+                }
+            });
+        });
+    }
+}
+
+
+ 
+        $('.owl-carousel4').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: false,
+            dots: true,
+            autoplay: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 1
+                },
+                1000: {
+                    items: 3
+                }
+            }
+        })
+$('.grid').imagesLoaded(function(){
+    $('.grid').isotope({
+      // options
+      itemSelector: '.grid-item',
+      layoutMode: 'fitRows'
+    });
+  });
+
+  $('.btn.all').click(function(){
+    $('.grid').isotope({
+      filter: '*'
+    })      
+  });
+
+  $('.btn.homepage').click(function(){
+    $('.grid').show('.homepage')
+  });
+
+  $('.btn.ui').click(function(){
+    $('.grid').isotope({
+      filter: '.ui'
+    })
+  });
+
+  $('.btn.banner').click(function(){
+    $('.grid').isotope({
+      filter: '.banner'
+    })
+  });
+
+
+  $('.btn').click(function(){
+    $('.btn').removeClass('on');
+    $(this).toggleClass('on');
+  });
+
+
+
+
+  
+
+});
